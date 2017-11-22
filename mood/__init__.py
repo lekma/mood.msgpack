36b571907c2b57d5761cba8903b65d890b6f9885
Malek <lekmalek@gmail.com>
2017-11-22 10:53:17 +0100

    add license block to __init__.py

--- mood/__init__.py
+++ mood/__init__.py
@@ -1,2 +1,24 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright © 2017 Malek Hadj-Ali
+# All rights reserved.
+#
+# This file is part of mood.
+#
+# mood is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3
+# as published by the Free Software Foundation.
+#
+# mood is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mood.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 __import__("pkg_resources").declare_namespace(__name__)
 
